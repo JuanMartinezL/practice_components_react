116fb1435bcf68b46e9685f98132474684f6c759
Juan Martinez <juanmart600@gmail.com>
2024-09-17 20:08:30 -0500

    my firts component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+    <div>MyFirstComponent</div>
+    <h1>Temas de React</h1>
+    <ul>
+        <li>Componentes</li>
+        <li>Eventos</li>
+        <li>Estados Hooks</li>
+        <li>Props</li>
+    </ul>
+    </>
+  )
+}
