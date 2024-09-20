4a24ba492f2f7957cf8c32f05c194b2e52a9a01f
Juan Martinez <juanmart600@gmail.com>
2024-09-19 21:09:55 -0500

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
+import { useState } from 'react';
 import './App.css';
-import { MyFirstComponent } from './components/MyFirstComponent';
-import { SecondComponent } from './components/SecondComponent';
+import { Child } from './components/Child';
+// import { MyFirstComponent } from './components/MyFirstComponent';
+// import { SecondComponent } from './components/SecondComponent';
+import { ThirdComponent } from './components/ThirdComponent';
 
 function App() {
-  
+
+  const [name, setName] = useState("Wendy Katherinne Gomez Villa");
 
   return (
-    <>
-      <h1>Fundamentos de React</h1>
-      <MyFirstComponent />
-      <SecondComponent />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Fundamentos de React</h1>
+        <hr className='white-hr'/>
+        {/* <MyFirstComponent /> */}
+        <hr className='white-hr'/>
+        {/* <SecondComponent /> */}
+        <hr className='white-hr'/>
+        <ThirdComponent
+          name="Juan"
+          last_name="Martinez"
+          age="25"
+        />
+        <hr className='white-hr'/>
+        <Child 
+          name={name}
+          setName={setName} 
+        />
+        <hr className='white-hr'/>
+      </header>
+    </div>
   )
-}
+};
 
 export default App
--- src/components/MyFirstComponent.jsx
+++ src/components/MyFirstComponent.jsx
@@ -14,7 +14,7 @@ export const MyFirstComponent = () => {
   return (
     <>
       <div>MyFirstComponent</div>
-      <div className="container bg-info-subtle mt-4">
+      <div className="container ">
         <h1>Temas de React</h1>
         <ul>
             <li>Componentes</li>
@@ -23,12 +23,12 @@ export const MyFirstComponent = () => {
             <li>Props</li>
         </ul>
       </div>
-      <div className="container bg-warning-subtle py-2">
+      <div className="container ">
         <h1>Datos del Docente</h1>
         <p>Nombre: <strong>{ name }</strong></p>
         <p>GitHub: { github_profile } </p>
       </div>
-      <div className="container bg-success-subtle py-2">
+      <div className="container ">
         <h1>Datos del Estudiante</h1>
         <p>Nombre: <strong>{ student.name }</strong></p>
         <p>Apellido: { student.last_name } </p>
